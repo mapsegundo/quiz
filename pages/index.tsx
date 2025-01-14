bb66c859d9ebfd62a48d05c9ba972fe9af58400f
Marshall Paiva <marshallpaiva@hotmail.com>
2025-01-14 11:35:26 -0300

    Atualização da BASE_URL para producao

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from "@/model/questao";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function Home() {
   const router = useRouter();
